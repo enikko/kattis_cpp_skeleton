2ddeaf746aef124c4aa0fe234a8870362f7804ac
Erik Nikko <65210465+enikko@users.noreply.github.com>
2022-01-21 13:01:58 +0100

    Added the initial code skeleton for the first problem

---
+++ src/problems/hello/hello.cpp
@@ -0,0 +1,9 @@
+/// @brief This file contains the solution to the hello kattis problem.
+
+#include <cstdio>
+
+/// @brief Prints "Hello World!" to standard out
+int main() {
+  // Your code goes here
+  return 0;
+}
